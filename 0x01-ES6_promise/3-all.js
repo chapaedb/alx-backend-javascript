9bb192198e6caf1f4b0ec377d6afeb3ce8efa521
chapaedb <chapaeresso1@gmail.com>
2024-06-24 22:27:01 +0300

    Update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,13 @@
-import {uploadPhoto, createUser} from './utils.js';
-export default function handleProfileSignup(){
-Promise.all([uploadPhoto(), createUser()])
-.then(results =>{
-    const [photo, user] = response;
+import { uploadPhoto, createUser } from './utils.js';
 
-    console.log(`${photo.body}` `${user.firstName}` `${user.lastName}`)
-} );
-.catch( error =>{
-    console.error('Signup system offline')
-});
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(results => {
+      const [photo, user] = results;
+
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(error => {
+      console.error('Signup system offline');
+    });
 }

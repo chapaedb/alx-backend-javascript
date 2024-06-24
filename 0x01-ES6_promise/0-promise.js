dad1d5eb230bb8db2f56b5b594e15496114c6429
chapaedb <chapaeresso1@gmail.com>
2024-06-24 15:14:09 +0300

    Added Task3

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-export default function getResponseFromAPI(response){
+export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        
+        resolve('Promise resolved');
     });
 }

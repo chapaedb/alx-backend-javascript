74debbce234aa1e619db337e1cf8eb72f223f87e
chapaedb <chapaeresso1@gmail.com>
2024-06-24 14:19:02 +0300

    Update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,13 @@
-export default getFullResponseFromAPI(success){
-    return new Promise((resolve, reject) => {
-        if(success) {
-            resolve({
-                status: 200,
-                body: 'Success',
-            });
-
-            
-        }
-        else{
-            reject ( new Error("The fake API is not working currently"));  
-        }
+// 1-promise.js
+export default function getFullResponseFromAPI(success) {
+return new Promise((resolve, reject) => {
+    if (success) {
+    resolve({
+        status: 200,
+        body: 'Success',
     });
+    } else {
+    reject(new Error('The fake API is not working currently'));
+    }
+});
 }

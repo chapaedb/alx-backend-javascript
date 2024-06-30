17bb09b16a439ee1a5ffe52aa653d63023e11e1e
chapaedb <chapaeresso1@gmail.com>
2024-06-30 11:54:29 +0300

    Added comments

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 export default class ClassRoom{
+    /**
+   * Implements a class named ClassRoom
+   * @param {Number} maxStudentsSize - maximum number of students in class
+   */
     constructor(maxStudentsSize){
         this.maxStudentsSize = _maxStudentsSize;
     }

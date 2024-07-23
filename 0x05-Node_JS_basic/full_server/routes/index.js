31d536af6a3ddb86ec7d4f3d5db76c970abf8653
chapaedb <chapaeresso1@gmail.com>
2024-07-24 00:42:26 +0300

    Update

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const StudentsController = require('../controllers/StudentsController');
+const AppController = require('../controllers/StudentsController');
+
+const router = express.Router();
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+module.exports = router;
---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = require('./routes/index.js');
+
+const app = express();
+
+const route = Router();
+app.route()
